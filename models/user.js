73d7316fe002380907fcc674b7ea6242faec84a4
Omar Kammouni <62219558+Omar-git22@users.noreply.github.com>
2021-04-30 21:44:09 +0300

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const passportLocalMongoose = require("passport-local-mongoose");
+
+
+const userSchema = new mongoose.Schema({
+    username:String,
+    password:String,
+}, {timestamps: true});
+
+userSchema.plugin(passportLocalMongoose);
+const User = mongoose.model('User', userSchema);
+module.exports = User;
