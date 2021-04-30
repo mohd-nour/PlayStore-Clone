1b1546f6489edb49b76e566005198c71c6daf735
Omar Kammouni <62219558+Omar-git22@users.noreply.github.com>
2021-04-30 22:18:07 +0300

    Update user.js

--- models/user.js
+++ models/user.js
@@ -4,6 +4,12 @@ const passportLocalMongoose = require("passport-local-mongoose");
 
 const userSchema = new mongoose.Schema({
     username:String,
+    wishlist: [{
+        type: String
+      }], // array of wishlist IDs
+      lastVisited: [{
+        type: String
+      }], // array of visited IDs
 }, {timestamps: true});
 
 userSchema.plugin(passportLocalMongoose);
