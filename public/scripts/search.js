787109bc3d5c557ac66dbecad790b4bfc3086730
Omar Kammouni <omk12@mail.aub.edu>
2021-05-03 15:08:09 +0300

    changes to categories onclick and footer location

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -2,8 +2,45 @@ window.onload = function(){
     document.getElementById("home_btn").addEventListener("click", home_click);
     document.getElementById("top_btn").addEventListener("click", top_click);
     document.getElementById("new_btn").addEventListener("click", new_click);
+};
+function home_click(){
+    remove_new_click();
+    remove_top_click();
+    document.getElementById("home_span").style.color = "black";
+    document.getElementById("home_span").style.fontWeight = "530";
+    document.getElementById("categ_home_write").style.borderBottom = "3px solid #689f38";
+};
+function remove_home_click(){
+    document.getElementById("home_span").style.color = "#737373";
+    document.getElementById("home_span").style.fontWeight = "lighter";
+    document.getElementById("categ_home_write").style.borderBottom = "none";
+}
+function top_click(){
+    remove_home_click();
+    remove_new_click();
+    document.getElementById("top_span").style.color = "black";
+    document.getElementById("top_span").style.fontWeight = "530";
+    document.getElementById("categ_top_write").style.borderBottom = "3px solid #689f38";
+};
+function remove_top_click(){
+    document.getElementById("top_span").style.color = "#737373";
+    document.getElementById("top_span").style.fontWeight = "lighter";
+    document.getElementById("categ_top_write").style.borderBottom = "none";
+};
+function new_click(){
+    remove_home_click();
+    remove_top_click();
+    document.getElementById("new_span").style.color = "black";
+    document.getElementById("new_span").style.fontWeight = "530";
+    document.getElementById("categ_new_write").style.borderBottom = "3px solid #689f38";
+};
+function remove_new_click(){
+    document.getElementById("new_span").style.color = "#737373";
+    document.getElementById("new_span").style.fontWeight = "lighter";
+    document.getElementById("categ_new_write").style.borderBottom = "none";
+};
     /* entertainment, apps, movies, books onclick begins */
-    document.getElementById("entertain_a").addEventListener("click", function(e){
+    /*document.getElementById("entertain_a").addEventListener("click", function(e){
         e.preventDefault();
         window.location.href = "/";
         document.getElementById("entertain_item").style.backgroundColor = "#444";
@@ -72,48 +109,12 @@ window.onload = function(){
         hover_apps_afterclick();
     });
     document.getElementById("profile_pic").addEventListener("click", function(){document.getElementById("prof_sign").style.display = "block";})
-    /* entertainment, apps, movies, books onclick ends */
-};
+    *//* entertainment, apps, movies, books onclick ends */
 /* home, top charts, new releases UI onclick begins*/
-function home_click(){
-    remove_new_click();
-    remove_top_click();
-    document.getElementById("home_span").style.color = "black";
-    document.getElementById("home_span").style.fontWeight = "530";
-    document.getElementById("categ_home_write").style.borderBottom = "3px solid #689f38";
-};
-function remove_home_click(){
-    document.getElementById("home_span").style.color = "#737373";
-    document.getElementById("home_span").style.fontWeight = "lighter";
-    document.getElementById("categ_home_write").style.borderBottom = "none";
-}
-function top_click(){
-    remove_home_click();
-    remove_new_click();
-    document.getElementById("top_span").style.color = "black";
-    document.getElementById("top_span").style.fontWeight = "530";
-    document.getElementById("categ_top_write").style.borderBottom = "3px solid #689f38";
-};
-function remove_top_click(){
-    document.getElementById("top_span").style.color = "#737373";
-    document.getElementById("top_span").style.fontWeight = "lighter";
-    document.getElementById("categ_top_write").style.borderBottom = "none";
-};
-function new_click(){
-    remove_home_click();
-    remove_top_click();
-    document.getElementById("new_span").style.color = "black";
-    document.getElementById("new_span").style.fontWeight = "530";
-    document.getElementById("categ_new_write").style.borderBottom = "3px solid #689f38";
-};
-function remove_new_click(){
-    document.getElementById("new_span").style.color = "#737373";
-    document.getElementById("new_span").style.fontWeight = "lighter";
-    document.getElementById("categ_new_write").style.borderBottom = "none";
-};
+
 /* home, top charts, new releases UI onclick ends*/
 
-/* not clicked begins */
+/* not clicked begins 
 function apps_notclicked(){
     document.getElementById("apps_item").style.backgroundColor = "rgba(243, 243, 243, 0.945)";//from here on changes other categs styles
     document.getElementById("apps_name").style.color = "#737373";
@@ -129,10 +130,10 @@ function books_notclicked(){
 function entertain_notclicked(){
     document.getElementById("entertain_item").style.backgroundColor = "rgba(243, 243, 243, 0.945)";//from here on changes other categs styles
     document.getElementById("entertain_name").style.color = "#737373";
-}
+}*/
 /* not clicked ends */
 
-/* hover while a categ is clicked */
+/* hover while a categ is clicked 
 function hover_entertain_afterclick(){
     document.getElementById("entertain_name").addEventListener("mouseover", function(){
         document.getElementById("entertain_item").style.backgroundColor = "#444";
@@ -174,4 +175,4 @@ function hover_books_afterclick(){
         document.getElementById("books_item").style.backgroundColor = "rgba(243, 243, 243, 0.945)";
         document.getElementById("books_name").style.color = "#737373";
     });
-}
+}*/
