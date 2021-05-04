df489d2c7ba76822531d126e4d3094c2f2ecd2e6
Nour El Arab <muhammadalarab@gmail.com>
2021-05-04 13:15:42 +0300

    refactor star system code

--- public/scripts/stars.js
+++ public/scripts/stars.js
@@ -1,1 +1,10 @@
+function getRating(rating) {
 
+  alert("here");
+
+  starsTotal = 5;
+
+  starPercentage = (rating / starsTotal) * 100;
+
+  return starPercentageRounded = Math.round(starPercentage / 10) * 10 + "%";
+}
